6083fd6d5a2f4ad119a56a1893cff6f0ce8f886c
Murilo <murilonunesneto@gmail.com>
2025-10-15 22:27:13 -0300

    Primeiro commit do servisor Node.js

---
+++ 02_utilizando_http/index.js
@@ -0,0 +1,17 @@
+const {createServer} = require('http');
+
+let server = createServer((reqest, response) => {
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(`
+        <h1> Hello World </h1> 
+        <p> Primeira página com Node.js </p>
+        `);
+    response.end();
+
+});
+
+server.listen(8000);
+
+console.log('Servidor ouvindo a porta 8000');
+
